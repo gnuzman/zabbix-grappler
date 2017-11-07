951a726208b09c0b3242aadc2ee92a7af211dd24
house <94644378@qq.com>
2017-11-07 11:08:15 +0800

    create

---
+++ src/main/java/com/zzh/zabbix/grappler/ZabbixGrapplerApplication.java
@@ -0,0 +1,12 @@
+package com.zzh.zabbix.grappler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZabbixGrapplerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZabbixGrapplerApplication.class, args);
+	}
+}
---
+++ src/test/java/com/zzh/zabbix/grappler/ZabbixGrapplerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.zzh.zabbix.grappler;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ZabbixGrapplerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
